480a855c2eb18a1ff177d00b0be7b9780a686c80
t4l-4 <talar.talha@gmail.com>
2026-06-10 18:00:56 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+// scroll animation
+const elements = document.querySelectorAll('[data-animate]');
+
+const observer = new IntersectionObserver(entries=>{
+  entries.forEach(entry=>{
+    if(entry.isIntersecting){
+      entry.target.style.opacity=1;
+      entry.target.style.transform='translateY(0)';
+    }
+  });
+});
+
+elements.forEach(el=>{
+  el.style.opacity=0;
+  el.style.transform='translateY(30px)';
+  el.style.transition='1s';
+  observer.observe(el);
+});
